a6228e090111e7e26ecd9819ca710eef5a2d8406
b-rucel <b-rucel@users.noreply.github.com>
2025-03-04 16:25:30 -0800

    adding logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,20 @@ import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "@/components/theme-provider"
+import logo from "../assets/artoo.png"
 
 export function Header({ onFolderTreeToggle }) {
   const { theme, setTheme } = useTheme()
 
   return (
-    <header className="border-b">
-      <div className="flex items-center justify-between px-6 h-14">
-        <button 
+    <header className="border-b relative overflow-hidden">
+      <div className="flex items-center justify-between px-6 h-14 relative z-10">
+        <button
           onClick={onFolderTreeToggle}
-          className="md:pointer-events-none" // Disable click on desktop
+          className="md:pointer-events-none"
         >
           <h1 className="text-2xl font-semibold tracking-tight flex items-center">
-            artoo
+            <img src={logo} alt="Artoo Logo" className="h-10 mr-2" /> artoo
             <Badge variant="secondary" className="ml-2 text-xs">beta</Badge>
           </h1>
         </button>
